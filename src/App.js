aa0ca4468daf24791c7e494f159f3d342ebe8119
DIGIPlus <admin@digiplus.cf>
2023-04-19 19:16:24 +0400

    Counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [count, setCount] = React.useState(0)
+  function add(){
+    setCount(prevCount => prevCount + 1 )
+  }
+    function subtract(){
+    setCount(prevCount => prevCount - 1 )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+      <button className='counter--minus' onClick={subtract}>-</button>
+    <div className='counter--count'>
+        <h1>{count}</h1> 
     </div>
-  );
+    <button className='counter--plus' onClick={add}>+</button>
+    </div>
+  )
 }
 
-export default App;
+
